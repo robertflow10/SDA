ee4496776934cc2d16aa2bbd29b6c5676e44e6a5
desktop-7ultdqt\sbaker <sbaker@vermilionit.co.uk>
2024-01-28 16:57:13 +0000

    Remove Singleton POS Strategy Singletons

--- Singleton/POS/POS-Strategy-Singletons/CalcTax.java
+++
@@ -1,9 +0,0 @@
-/*
- * this is the required abstract class for Strategy DP
- */
-public abstract class CalcTax {
-    float taxRate;
-    public CalcTax() {}
-
-    abstract public float getTaxAmount(float value);
-}
