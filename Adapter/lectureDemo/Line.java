6bedbb6d44f3ce40d01fddf72576fcf4fdb641f3
desktop-7ultdqt\sbaker <sbaker@vermilionit.co.uk>
2024-01-28 16:54:14 +0000

    Remove Adaptor lecture demo solution

--- Adapter/lectureDemo/Line.java
+++
@@ -1,7 +0,0 @@
-public class Line {
-    int startX, startY, finishX, finishY;
-    // methods
-    public void draw(int x1, int y1, int x2, int y2) {
-        System.out.printf("draw from (%d,%d) to (%d,%d)\n", x1,y1,x2,y2);
-    }
-}
