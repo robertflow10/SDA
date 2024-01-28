1cd4da4b83d1bc0d667f3831c82290d2b687177b
desktop-7ultdqt\sbaker <sbaker@vermilionit.co.uk>
2024-01-28 16:54:49 +0000

    Remove Bridge (3LP) solution

--- Bridge/ThreeLittlePigs/bridgeSolution/Bricks.java
+++
@@ -1,8 +0,0 @@
-public class Bricks extends MaterialsService {
-    public Bricks() {
-	System.out.println("using bricks");
-    }
-    public void buildWithMaterials() {
-	System.out.println("Piggy lays rows of bricks on top of each other");
-    }
-}
--- Bridge/ThreeLittlePigs/bridgeSolution/MaterialsService.java
+++
@@ -1,3 +0,0 @@
-public abstract class MaterialsService {
-    public abstract void buildWithMaterials();
-}
