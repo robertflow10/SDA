192b02b260e1d008c6bec0245c10b054f5642c8e
desktop-7ultdqt\sbaker <sbaker@vermilionit.co.uk>
2024-01-28 16:56:42 +0000

    Remove Facade HomeMoveFacade Solution

--- Facade/HomeMovieFacade/Projector.java
+++
@@ -1,8 +0,0 @@
-public class Projector {
-    public void on() {
-	System.out.println("Projecter ON");
-    }
-    public void off() {
-	System.out.println("Projecter OFF");
-    }
-}
--- Facade/HomeMovieFacade/Screen.java
+++
@@ -1,8 +0,0 @@
-public class Screen {
-    public void lower() {
-	System.out.println("Screen lowered");
-    }
-    public void raise() {
-	System.out.println("Screen raised");
-    }
-}
